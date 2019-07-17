1fab5ea05067b96a3df99afe9ab05c0c651ae420
Steven Terwilliger <steventwig@gmail.com>
2019-07-16 21:42:00 -0700

    .js working moving to css

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+var countDownValue = 5;
+var onScreenQuestion = 0;
+var wins = 0;
+var losses = 0;
+var timer;
+
+function countDown(){
+    countDownValue--;
+    $('#timer').html("Count Down:" + countDownValue);
+    if (countDownValue === 0){
+        questionTimeOut()
+    }
+}
+function questionTimeOut(){
+    clearInterval(timer);
+    losses++;
+    loadNextQuestion()
+}
+function winLossDisplay(){
+    var winText = '<p>You got ' + wins + ' correct answers</p>';
+    var lossText = '<p>You got ' + losses + ' incorrect answers</p>';
+    var thanksText = '<p>Thank you for playing</p>';
+    var playAgainButton = '<button id="restartButton" class="btn-dark">Play again</button>';
+    var imDoneButton = '<button id="imDone" class="btn-dark">Im done</button>'
+    $('#timer').html('Score screen');
+    $('#gameArea').html(winText + lossText + thanksText + playAgainButton + imDoneButton);
+}
+function loadNextQuestion(){
+    var endOfQuestion = (triviaQuestions.length - 1) === onScreenQuestion;
+    if(endOfQuestion){
+        winLossDisplay();
+    } else{
+        onScreenQuestion++;
+        showQuestions();
+    }
+}
+function showChoices(avalibleChoices) {
+    for (var currentChoice = 0; currentChoice < avalibleChoices.length; currentChoice++) {
+        $('#gameArea').append('<p class="selectedAnswer" data-id="' + currentChoice + '">' + avalibleChoices[currentChoice] + '</p>');
+    }
+}
+function showQuestions() {
+    countDownValue = 5;
+    timer =setInterval(countDown, 1000);
+    var tempQuestion = triviaQuestions[onScreenQuestion].askedQuestion;
+    var tempChoices = triviaQuestions[onScreenQuestion].avalibleChoices;
+    $('#timer').html("Count Down:" + countDownValue);
+    $('#gameArea').html('<h4>' + tempQuestion + '</h4>');
+    showChoices(tempChoices);
+}
+
+$(document).on('click','.selectedAnswer',function(){
+    clearInterval(timer);
+    var chosenAnswerIndex = $(this).attr('data-id');
+    var chosenAnswer = triviaQuestions[onScreenQuestion].avalibleChoices[chosenAnswerIndex];
+    var rightAnswer = triviaQuestions[onScreenQuestion].correctAnswer;
+    if (rightAnswer === chosenAnswer) {
+        console.log("Correct");
+        wins++;
+    } else {
+        console.log("Incorrect");
+        losses++;
+    }
+    loadNextQuestion();
+});
+$(document).on('click','#restartButton',function(){
+    onScreenQuestion = 0;
+    wins = 0;
+    losses = 0;
+    showQuestions();
+});
+$(document).on('click','#imDone',function(){
+    $('#timer').empty();
+    $('#gameArea').empty();
+});
+$(document).on('click','#start',function(){
+    onScreenQuestion = 0;
+    wins = 0;
+    losses = 0;
+    showQuestions();
+});
---
+++ assets/javascript/questions.js
@@ -0,0 +1,52 @@
+var triviaQuestions = [
+    {
+        askedQuestion: "1 2 or 3",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "1",
+    },
+    {
+        askedQuestion: "4 5 or 6",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "3",
+    },
+    {
+        askedQuestion: "7 8 or 9",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "2",
+    },
+    {
+        askedQuestion: "1 2 or 3",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "1",
+    },
+    {
+        askedQuestion: "4 5 or 6",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "3",
+    },
+    {
+        askedQuestion: "7 8 or 9",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "2",
+    },
+    {
+        askedQuestion: "1 2 or 3",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "1",
+    },
+    {
+        askedQuestion: "4 5 or 6",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "3",
+    },
+    {
+        askedQuestion: "7 8 or 9",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "2",
+    },
+    {
+        askedQuestion: "7 8 or 9",
+        avalibleChoices: ["1","2","3"],
+        correctAnswer: "2",
+    },
+];
